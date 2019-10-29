e216e641230e672ef1f261df8d2b9d2f337ff01d
DannyBoyNYC <daniel.deverell@gmail.com>
2019-10-28 21:35:50 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import Pirate from './components/Pirate';
 import Header from './components/Header';
+import PirateForm from './components/PirateForm';
+
 import piratesFile from './data/sample-pirates-array';
 
-const pirateCalls = [
-  'Aaarg, belay that!',
-  'Avast me hearties!',
-  'Shiver me timbers!',
-];
+class App extends React.Component {
+  state = {
+    pirates: piratesFile,
+  };
 
-const randomize = () =>
-  pirateCalls[Math.floor(Math.random() * pirateCalls.length)];
+  render() {
+    const pirateCalls = [
+      'Aaarg, belay that!',
+      'Avast me hearties!',
+      'Shiver me timbers!',
+    ];
 
-function App() {
-  return (
-    <div>
-      <Header title={randomize()} />
-      {piratesFile.map(pirate => (
-        <Pirate key={pirate.id} tagline={randomize()} pirate={pirate} />
-      ))}
-    </div>
-  );
+    const addPirate = pirate => {
+      console.log(pirate);
+      //take a copy of the current state and put it into newPirates var
+      const newPirates = [...this.state.pirates];
+      console.log(newPirates);
+      newPirates.unshift(pirate);
+      console.log(newPirates);
+      //set state pirates with var pirates
+      this.setState({ pirates: newPirates });
+    };
+
+    const randomize = () =>
+      pirateCalls[Math.floor(Math.random() * pirateCalls.length)];
+
+    return (
+      <>
+        <Header title={randomize()} />
+        <PirateForm addPirate={addPirate} />
+        {this.state.pirates.map(pirate => (
+          <Pirate key={pirate.id} tagline={randomize()} pirate={pirate} />
+        ))}
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Pirate.js
+++ src/components/Pirate.js
@@ -2,34 +2,51 @@ import React from 'react';
 import '../assets/css/Pirate.css';
 import avatar from '../assets/img/avatar.png';
 
-// function Pirate(props) {
-//   return (
-//     <div className="pirate">
-//       <p>{props.tagline}</p>
-//     </div>
-//   );
-// }
-
-class Pirate extends React.Component {
-  render() {
-    return (
-      <div className="pirate">
+function Pirate({ tagline, pirate: { desc, name, year, weapon, vessel } }) {
+  return (
+    <main>
+      <aside className="pirate-data">
         <ul>
           <li>
             <img src={avatar} alt="pirate" />
-            <h3>{this.props.pirate.name}</h3>
-            <p>Died: {this.props.pirate.year}</p>
-            <p>Favorite weapon: {this.props.pirate.weapon}</p>
-            <p>Sailed on: {this.props.pirate.vessel}</p>
           </li>
           <li>
-            <h2>"{this.props.tagline}"</h2>
-            <p>{this.props.pirate.desc}</p>
+            <h3>{name}</h3>
           </li>
+          <li>Died: {year}</li>
+          <li>Favorite weapon: {weapon}</li>
+          <li>Sailed on: {vessel}</li>
         </ul>
-      </div>
-    );
-  }
+      </aside>
+      <article>
+        <h2>"{tagline}"</h2>
+        <p>{desc}</p>
+      </article>
+    </main>
+  );
 }
+
+// class Pirate extends React.Component {
+//   render() {
+//     const { name, year, weapon, vessel, desc } = this.props.pirate;
+//     return (
+//       <div className="pirate">
+//         <ul>
+//           <li>
+//             <img src={avatar} alt="pirate" />
+//             <h3>{name}</h3>
+//             <p>Died: {year}</p>
+//             <p>Favorite weapon: {weapon}</p>
+//             <p>Sailed on: {vessel}</p>
+//           </li>
+//           <li>
+//             <h2>"{this.props.tagline}"</h2>
+//             <p>{desc}</p>
+//           </li>
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
 
 export default Pirate;
